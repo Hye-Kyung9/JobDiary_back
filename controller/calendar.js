935de20c3d4fa8c09b0d7a065dc9f4b758fb07d2
Hye-Kyung9 <rngprud123@naver.com>
2022-06-10 17:12:40 +0900

    Add removeEvent

--- controller/calendar.js
+++ controller/calendar.js
@@ -6,6 +6,10 @@ import moment from "moment";
 const router = express.Router();
 
 router.post("/create-event", async (req, res) => {
+  if (req.body["end"] == null) {
+    req.body["end"] = req.body.start;
+  }
+
   const event = Event(req.body);
   await event.save();
   res.sendStatus(201);
@@ -17,6 +21,11 @@ router.get("/get-events", async (req, res) => {
     end: { $lte: moment(req.query.end).toDate() },
   });
   res.send(events);
+});
+
+router.get("/remove-event", async (req, res) => {
+  await Event.deleteOne({ id: req.query.id });
+  res.sendStatus(201);
 });
 
 export default router;
